42f935e211bb63b795678a8dbb1ad471a50df57e
Jay Ramoliya <jankipateljp03@gmail.com>
2024-12-01 10:20:15 +0530

    2703. Return Length of Arguments Passed

---
+++ 2703. Return Length of Arguments Passed.js
@@ -0,0 +1,24 @@
+// DAY 9
+// 2703. Return Length of Arguments Passed (01-12-2024)
+
+// Write a function argumentsLength that returns the count of arguments passed to it.
+
+// Input: args = [5]
+// Output: 1
+// Explanation:
+// argumentsLength(5); // 1
+
+// One value was passed to the function so it should return 1.
+// Example 2:
+
+// Input: args = [{}, null, "3"]
+// Output: 3
+// Explanation: 
+// argumentsLength({}, null, "3"); // 3
+
+// Three values were passed to the function so it should return 3.
+
+var argumentsLength = function(...args) {
+    return args.length;
+};
+console.log(argumentsLength({}, null, "3"));
